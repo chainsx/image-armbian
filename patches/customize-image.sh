160af101ba2bff54a8b602c6951f09b8684cff6e
chainsx <pzhrhlx@outlook.com>
2023-02-12 22:40:36 +0800

    Create customize-image.sh

---
+++ patches/customize-image.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get install software-properties-common
+sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
+sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
+apt-get update && apt-get upgrade -y
+apt-get clean
