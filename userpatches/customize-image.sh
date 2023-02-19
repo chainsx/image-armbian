5eb7d3acb999a815a6cbccbe9d39f93e68f6e3da
chainsx <pzhrhlx@outlook.com>
2023-02-19 15:17:02 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -1,5 +1,8 @@
 apt-get update && apt-get install software-properties-common
-sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
 sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
 apt-get update && apt-get upgrade -y
+sudo apt install rockchip-multimedia-config
 apt-get clean
+
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
