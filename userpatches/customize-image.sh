5522f060db64a536612f0fe78e4de55171dbe1b6
chainsx <pzhrhlx@outlook.com>
2023-03-21 17:44:07 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -2,6 +2,7 @@ apt-get update && apt-get install software-properties-common
 sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
 sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
 apt-get update && apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 sudo apt install rockchip-multimedia-config mali-g610-firmware libmali-g610-x11 libgl4es
 apt-get clean
 
