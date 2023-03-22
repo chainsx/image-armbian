f95af803420088f482ee011dbd68c4cceaa016e6
chainsx <pzhrhlx@outlook.com>
2023-03-22 13:13:30 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
 sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
 apt-get update && apt-get upgrade -y
 apt-get update && apt-get upgrade -y
-sudo apt install rockchip-multimedia-config mali-g610-firmware libmali-g610-x11 libgl4es
+sudo apt install rockchip-multimedia-config mali-g610-firmware libmali-g610-x11 libgl4es -y
 apt-get clean
 
 update-alternatives --set iptables /usr/sbin/iptables-legacy
