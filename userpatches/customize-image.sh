8704a31835ab2441f0006502d12287896a2ff05c
青菜萝 卜冬瓜 <chainsx@outlook.com>
2024-07-28 21:36:02 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -4,7 +4,12 @@ sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
 apt-get update && apt-get upgrade -y
 apt-get update && apt-get upgrade -y
 sudo apt install rockchip-multimedia-config mali-g610-firmware libmali-g610-x11 libgl4es -y
+apt-get update && apt-get upgrade -y
 apt-get clean
 
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+sed -i "s|ports.ubuntu.com|mirrors.ustc.edu.cn/ubuntu-ports|g" /etc/apt/sources.list
+sed -i "s|ports.ubuntu.com|launchpad.proxy.ustclug.org|g" /etc/apt/sources.list.d/liujianfeng1994-ubuntu-panfork-mesa-jammy.list
+sed -i "s|ports.ubuntu.com|launchpad.proxy.ustclug.org|g" /etc/apt/sources.list.d/liujianfeng1994-ubuntu-rockchip-multimedia-jammy.list
